3c127b7d71a44a394497ada5548971af33bee43a
Yoseph Tamene <tameney22@mail.wlu.edu>
2020-12-31 19:34:53 -0800

    Edited some more

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   let index = main();
-  core.setOutput("index", "Checking again");
+  core.setOutput("index", "it works!");
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify(github.context.payload, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
