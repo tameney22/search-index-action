d7b80cb074cb3fec6b2a1bcfd37b0a1867ecb93c
Yoseph Tamene <tameney22@mail.wlu.edu>
2020-12-31 18:58:18 -0800

    Change output variable name

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ try {
 //   const time = (new Date()).toTimeString();
 //   core.setOutput("time", time);
   let index = main();
-  core.setOutput("searchIndex", index);
+  core.setOutput("index", index);
   // Get the JSON webhook payload for the event that triggered the workflow
 //   const payload = JSON.stringify(github.context.payload, undefined, 2)
 //   console.log(`The event payload: ${payload}`);
